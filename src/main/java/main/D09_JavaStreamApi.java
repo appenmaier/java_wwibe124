847542f9166635fb72fe21d6a4543e4461dbd804
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2025-05-22 09:30:46 +0200

    Update Demo 9 (Java Stream API)

--- src/main/java/main/D09_JavaStreamApi.java
+++ src/main/java/main/D09_JavaStreamApi.java
@@ -1,9 +1,14 @@
 package main;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 import model.Movie;
 import model.Movie.Genre;
@@ -40,30 +45,111 @@ public class D09_JavaStreamApi {
       /* max/min/count */
       getLongestMovie().ifPresent(System.out::println);;
       System.out.println();
+
+      /* filter, findAny/findFirst */
+      getDrama().ifPresent(System.out::println);
+      System.out.println();
+
+      /* anyMatch/allMatch/noneMatch */
+      System.out.println(doesExistMovieBefore1982());
+      System.out.println();
+
+      /* map, distinct, sorted, toList */
+      allYearsAsSortedList().forEach(System.out::println);
+      System.out.println();
+
+      /* collect */
+      getAllMoviesByYear().forEach((year, movies) -> System.out.println(year + ": " + movies));
+      System.out.println();
+
+      /* Java Stream API bei Maps */
+      Map<String, List<Movie>> allMoviesByYear = getAllMoviesByYear();
+
+      Optional<String> year = allMoviesByYear.keySet().stream().findAny();
+      year.ifPresent(System.out::println);
+
+      long numberOfValues =
+            allMoviesByYear.values().stream().filter(movies -> movies.size() > 2).count();
+      System.out.println(numberOfValues);
+
+      allMoviesByYear.entrySet()
+            .stream()
+            .filter(entry -> entry.getValue().size() > 2)
+            .map(Entry::getKey)
+            .forEach(System.out::println);
+   }
+
+   private static Map<String, List<Movie>> getAllMoviesByYear() {
+      System.out.println("Alle Filme gruppiert nach Jahr: ");
+
+      Map<String, List<Movie>> allMoviesByYear = new HashMap<>();
+      allMoviesByYear = movies.stream().collect(Collectors.groupingBy(Movie::year)); // .collect(Collectors.groupingBy(movie
+                                                                                     // ->
+                                                                                     // movie.year()));
+      return allMoviesByYear;
+   }
+
+   private static List<String> allYearsAsSortedList() {
+      System.out.println("Alle Jahre: ");
+
+      List<String> allYears = new ArrayList<>();
+      allYears = movies.stream()
+            .map(Movie::year) // .map(movie -> movie.year())
+            .distinct()
+            .sorted()
+            .toList()
+            .reversed();
+      return allYears;
+   }
+
+   private static boolean doesExistMovieBefore1982() {
+      System.out.print("Film vor 1982?: ");
+
+      boolean exists = false;
+      exists = movies.stream().anyMatch(movie -> movie.year().compareTo("1982") < 0);
+      return exists;
+   }
+
+   private static Optional<Movie> getDrama() {
+      System.out.print("Ein Drama: ");
+
+      Optional<Movie> drama = Optional.empty();
+      drama = movies.stream().filter(movie -> movie.genres().contains(Genre.DRAMA)).findAny(); // .findFirst()
+      return drama;
    }
 
    private static Optional<Movie> getLongestMovie() {
       System.out.print("Der längste Film: ");
-      Optional<Movie> longestMovie = Optional.empty();
-      longestMovie = movies.stream()
-            .max((m1, m2) -> Integer.compare(m1.runtimeInMinutes(), m2.runtimeInMinutes()));
-      return longestMovie;
+
+      /* Variante A */
+      // Optional<Movie> longestMovie = Optional.empty();
+      // longestMovie = movies.stream()
+      // .max((movie1, movie2) -> Integer.compare(movie1.runtimeInMinutes(),
+      // movie2.runtimeInMinutes()));
+      // return longestMovie;
+
+      /* Variante B */
+      return movies.stream()
+            .max((movie1, movie2) -> Integer.compare(movie1.runtimeInMinutes(),
+                  movie2.runtimeInMinutes()));
    }
 
    private static void printTop5HorrorMovies() {
       System.out.println("Die Top 5 Horror-Filme:");
+
       movies.stream()
-            .filter(m -> m.genres().contains(Genre.HORROR))
-            .sorted((m1, m2) -> Double.compare(m2.rating(), m1.rating()))
+            .filter(movie -> movie.genres().contains(Genre.HORROR))
+            .sorted((movie1, movie2) -> Double.compare(movie2.rating(), movie1.rating()))
             .limit(5)
             .forEach(System.out::println);
    }
 
    private static OptionalDouble getAverageOfAllMovies() {
       System.out.print("Die Durchschnittsbewertung aller Filme: ");
+
       OptionalDouble averageOfAllMovies = OptionalDouble.empty();
       averageOfAllMovies = movies.stream()
-            .mapToDouble(m -> m.rating()) // .mapToDouble(Movie::rating)
+            .mapToDouble(movie -> movie.rating()) // .mapToDouble(Movie::rating)
             .average();
       return averageOfAllMovies;
    }
@@ -71,14 +157,15 @@ public class D09_JavaStreamApi {
    private static void printAllThrillersWithRatingGE7() {
       System.out
             .println("Alle Thriller mit einer Bewertung von min. 7 in der Form [Title] ([Jahr]):");
+
       movies.stream()
-            .filter(m -> m.genres().contains(Genre.THRILLER))
-            .filter(m -> m.rating() >= 7)
+            .filter(movie -> movie.genres().contains(Genre.THRILLER))
+            .filter(movie -> movie.rating() >= 7)
             /* Variante A */
-            .map(m -> m.title() + " (" + m.year() + ")")
+            .map(movie -> movie.title() + " (" + movie.year() + ")")
             .forEach(System.out::println);
       /* Variante B */
-      // .forEach(m -> System.out.println(m.title() + " (" + m.year() + ")"));
+      // .forEach(movie -> System.out.println(movie.title() + " (" + movie.year() + ")"));
    }
 
 }
