557e37c9d78e426e3e95d6ec7ead36bc8d741f9a
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2025-06-05 12:14:59 +0200

    Add UnitTest for Shop and Add Java Stream API Solutions for Shop

--- src/main/java/model/Shop.java
+++ src/main/java/model/Shop.java
@@ -1,11 +1,11 @@
 package model;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
+import java.util.OptionalDouble;
 
 /**
  * Shop
@@ -43,26 +43,26 @@ public record Shop<T extends Comparable<T>>(String name, Map<T, List<Integer>> a
    public Optional<T> getBestRatedProduct() {
       // 0,5
       /* Variante A */
-      T bestRatedProduct = null; // 0,5
-      double bestAverageRating = 0; // 0,5
-
-      for (Entry<T, List<Integer>> entry : assortment.entrySet()) { // 1
-         T product = entry.getKey(); // 0,5
-         List<Integer> ratings = entry.getValue(); // 0,5
-
-         double total = 0; // 0,5
-         for (int rating : ratings) { // 1
-            total += rating; // 0,5
-         }
-         double averageRating = total / ratings.size(); // 1
-
-         if (averageRating >= bestAverageRating) { // 1
-            bestAverageRating = averageRating; // 0,5
-            bestRatedProduct = product; // 0,5
-         }
-      }
-
-      return Optional.ofNullable(bestRatedProduct); // 1,5
+      // T bestRatedProduct = null; // 0,5
+      // double bestAverageRating = 0; // 0,5
+      //
+      // for (Entry<T, List<Integer>> entry : assortment.entrySet()) { // 1
+      // T product = entry.getKey(); // 0,5
+      // List<Integer> ratings = entry.getValue(); // 0,5
+      //
+      // double total = 0; // 0,5
+      // for (int rating : ratings) { // 1
+      // total += rating; // 0,5
+      // }
+      // double averageRating = total / ratings.size(); // 1
+      //
+      // if (averageRating >= bestAverageRating) { // 1
+      // bestAverageRating = averageRating; // 0,5
+      // bestRatedProduct = product; // 0,5
+      // }
+      // }
+      //
+      // return Optional.ofNullable(bestRatedProduct); // 1,5
 
       /* Variante B */
       // Optional<T> bestRatedProduct = Optional.empty(); // 1
@@ -85,25 +85,59 @@ public record Shop<T extends Comparable<T>>(String name, Map<T, List<Integer>> a
       // }
       //
       // return bestRatedProduct; // 0,5
+
+      /* Variante C */
+      // T bestRatedProduct = null; // 0,5
+      // double bestAverageRating = 0; // 0,5
+      //
+      // for (Entry<T, List<Integer>> entry : assortment.entrySet()) { // 1
+      // T product = entry.getKey(); // 0,5
+      // List<Integer> ratings = entry.getValue(); // 0,5
+      //
+      // OptionalDouble averageRating = ratings.stream().mapToInt(r -> r).average(); // 2,5
+      //
+      // if (averageRating.isPresent() && averageRating.getAsDouble() >= bestAverageRating) { // 1,5
+      // bestAverageRating = averageRating.getAsDouble(); // 0,5
+      // bestRatedProduct = product; // 0,5
+      // }
+      // }
+      //
+      // return Optional.ofNullable(bestRatedProduct); // 1,5
+
+      /* Variante D */
+      return assortment.entrySet() // 1
+            .stream()// 0,5
+            .sorted((e1, e2) -> { // 0,5
+               OptionalDouble optional1 = e1.getValue().stream().mapToInt(r -> r).average(); // 1,5
+               OptionalDouble optional2 = e2.getValue().stream().mapToInt(r -> r).average(); // 1,5
+               Double averageRating1 = optional1.isPresent() ? optional1.getAsDouble() : 0; // 1
+               Double averageRating2 = optional2.isPresent() ? optional2.getAsDouble() : 0; // 1
+               return averageRating2.compareTo(averageRating1); // 1
+            })
+            .map(Entry::getKey) // 1
+            .findFirst(); // 0,5
    } // 10
 
    public List<T> getAllProductsSortedByNaturalOrdering() {
       // 0,5
       /* Variante A */
-      List<T> products = new ArrayList<>(); // 0,5
-
-      for (T product : assortment.keySet()) { // 1
-         products.add(product); // 0,5
-      }
-
-      Collections.sort(products); // 1
-      return products; // 0,5
+      // List<T> products = new ArrayList<>(); // 0,5
+      //
+      // for (T product : assortment.keySet()) { // 1
+      // products.add(product); // 0,5
+      // }
+      //
+      // Collections.sort(products); // 1
+      // return products; // 0,5
 
       /* Variante B */
       // List<T> products = new ArrayList<>(assortment.keySet()); // 2
       //
       // Collections.sort(products); // 1
       // return products; // 0,5
+
+      /* Variante C */
+      return assortment.keySet().stream().sorted().toList(); // 3,5
    } // 4
 
 } // 25,5
---
+++ src/test/java/model/ShopTest.java
@@ -0,0 +1,91 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+/**
+ * ShopTest
+ *
+ * @author Daniel Appenmaier
+ * @version 1.0
+ *
+ */
+public class ShopTest {
+
+   private Shop<FastFood> shop;
+   @Mock
+   private FastFood fastFood1;
+   @Mock
+   private FastFood fastFood2;
+   @Mock
+   private FastFood fastFood3;
+
+   @BeforeEach
+   void setUp() {
+      MockitoAnnotations.openMocks(this);
+
+      shop = new Shop<>("Burger Queen", new HashMap<>());
+   }
+
+   @Test
+   void testAddProduct() {
+      shop.addProduct(fastFood1);
+      shop.addProduct(fastFood2);
+      shop.addProduct(fastFood3);
+      shop.addProduct(fastFood1);
+
+      assertEquals(3, shop.assortment().keySet().size());
+      assertTrue(shop.assortment().containsKey(fastFood1));
+      assertTrue(shop.assortment().containsKey(fastFood2));
+      assertTrue(shop.assortment().containsKey(fastFood3));
+   }
+
+   @Test
+   void testRateProduct() {
+      shop.addProduct(fastFood1);
+      shop.addProduct(fastFood2);
+
+      assertThrows(NoProductFoundException.class, () -> shop.rateProduct(fastFood3, 4));
+      assertThrows(InvalidRatingException.class, () -> shop.rateProduct(fastFood2, 0));
+      assertThrows(InvalidRatingException.class, () -> shop.rateProduct(fastFood2, 10));
+   }
+
+   @Test
+   void testGetBestRatedProduct() {
+      shop.assortment().put(fastFood1, List.of(2));
+      shop.assortment().put(fastFood2, List.of(5, 3, 4));
+      shop.assortment().put(fastFood3, List.of(1, 5, 1, 5, 1));
+
+      assertEquals(Optional.of(fastFood2), shop.getBestRatedProduct());
+   }
+
+   @Test
+   void testGetAllProductsSortedByNaturalOrdering() {
+      shop.assortment().put(fastFood1, new ArrayList<>());
+      shop.assortment().put(fastFood2, new ArrayList<>());
+      shop.assortment().put(fastFood3, new ArrayList<>());
+
+      when(fastFood1.compareTo(fastFood2)).thenReturn(-1);
+      when(fastFood1.compareTo(fastFood3)).thenReturn(-1);
+      when(fastFood2.compareTo(fastFood1)).thenReturn(1);
+      when(fastFood2.compareTo(fastFood1)).thenReturn(1);
+      when(fastFood3.compareTo(fastFood1)).thenReturn(1);
+      when(fastFood3.compareTo(fastFood2)).thenReturn(-1);
+
+      assertEquals(List.of(fastFood1, fastFood3, fastFood2),
+            shop.getAllProductsSortedByNaturalOrdering());
+   }
+
+}
